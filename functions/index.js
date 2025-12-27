d5df0222091b8226e8d10ab3a11e6e94064d9bc0
wn.lin <wn.lin@104.com.tw>
2025-12-28 03:46:26 +0800

    chore: update site

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,19 @@
 import * as latestCommitHandler from './api/latest-commit.js';
 
 export const onRequest = async (context) => {
-  const url = new URL(context.request.url);
-  if (url.pathname.startsWith('/api/latest-commit')) {
-    return latestCommitHandler.onRequest(context);
+  try {
+    const url = new URL(context.request.url);
+    if (url.pathname.startsWith('/api/latest-commit')) {
+      return latestCommitHandler.onRequest(context);
+    }
+    if (typeof context.next === 'function') {
+      return context.next();
+    }
+    return new Response('Not found', { status: 404 });
+  } catch (error) {
+    console.error('Function error', error);
+    return new Response('Internal server error', { status: 500 });
   }
-  return new Response('Not found', { status: 404 });
 };
 
 export default {
